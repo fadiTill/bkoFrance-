bb88b0db1af437e2af416bfe37c4d9dd4cd7103f
Fadimata Tillman <fadi.tillman@gmail.com>
2021-10-20 13:20:50 -0700

    create datapillows file to update static datas

--- frontendbkofr/README.md
+++ frontendbkofr/README.md
@@ -16,4 +16,26 @@
   5. style html, body
   6. style grid-container, header, main and footer
 
-  3.
+  3.Create Static Home screen
+     1.create ul.products
+     2.create li
+     3.create div.product
+     4.add .product-image, .product-name, .product-brand, .product-price
+     5.style ul.products and internal divs
+     6. duplicate 
+
+
+     4.Render Dynamic Home Screen
+       1.create data.js
+       2.export an array of 6 products
+       3.create screen/HomeScreen.js
+       4.export HomeScreen as an object with render() method
+       5.implement render()
+       6.import data.js
+       7.return products mapped to lis inside an ul 
+       8.create app.js
+       9.link it to index.html as module
+       10.set main id to main container
+       11. create router()function
+       12.set main_container innerHTML to HomeScreen.render()
+       13. set load event of window to router() function
---
+++ frontendbkofr/src/Screen/HomeScreen.js
@@ -0,0 +1,77 @@
+
+import data from'../dataPillows.js'
+const HomeScreen = {
+    render: () => {
+        const{ products } = dataPillows;
+return `
+<ul class="products">
+${products.map(product =>`
+
+<li>
+        <div class="container">
+            <div class="card">
+              <div class="card-head">
+                 
+                <img src="images/transparent copy.png" alt="logo" class="card-logo">
+                <img src="${product.image}" alt="${product.category}" class="product-img">
+           
+                <div class="product-detail">
+                
+                </div>
+                <span class="back-text">
+                        BKO
+                        Fashion
+                      </span>
+              </div>
+              <div class="card-body">
+                <div class="product-desc">
+                  <span class="product-title">
+                    <a href="/#/product/${product.id}">${product.title}</a>
+                          <span class="badge">
+                           
+                          </span>
+                  </span>
+                  <span class="product-caption">
+                    100%  Organic 
+                  </span>
+                  <span class="product-rating">
+                          <i class="fa fa-star"></i>
+                          <i class="fa fa-star"></i>
+                          <i class="fa fa-star"></i>
+                          <i class="fa fa-star"></i>
+                          <i class="fa fa-star grey"></i>
+                        </span>
+                        <div class="prix">
+                           ${product.price}
+                        </div>
+                </div>
+                <div class="product-properties">
+                  <span class="product-size">
+                          <h4>Size</h4>
+                          <ul class="ul-size">
+                            <li><a href="#">7</a></li>
+                            <li><a href="#">8</a></li>
+                            <li><a href="#">9</a></li>
+                            <li><a href="#" class="active">10</a></li>
+                            <li><a href="#">11</a></li>
+                          </ul>
+                        </span>
+                  <span class="product-color">
+                          <h4>${product.color}</h4> 
+                         
+                          
+                        </span>
+                       
+                  <span class="product-price">
+                    <b>Acheter</b>
+                        </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+</li>
+`)}
+`
+    }
+}
